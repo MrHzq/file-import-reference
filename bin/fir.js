a43e01f1539df0a10c5863dc8ea929df4cfc9dc9
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-07-29 16:26:19 +0800

    feat: add version

--- bin/fir.js
+++ bin/fir.js
@@ -5,10 +5,15 @@
 // 引入 commander 模块，官方使用文档：https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md
 const { program } = require("commander");
 
+const version = require("../package.json").version;
+
 // 定义命令与参数
-program.description("find file import reference").action((a1, options) => {
-  require("../lib/file-import-reference")(options);
-});
+program
+  .version(version)
+  .description("find file import reference")
+  .action((a1, options) => {
+    require("../lib/file-import-reference")(options);
+  });
 
 // 解析用户输入的命令和参数，第一个参数是要解析的字符串数组，第二个参数是解析选项
 program.parse(process.argv); // 指明，按 node 约定
